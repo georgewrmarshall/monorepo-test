b048dd77c0255fa6f1bcbf1155749c5eedb10d92
georgewrmarshall <george.marshall@consensys.net>
2024-09-18 11:25:27 -0700

    chore: update to ui

--- packages/ui/index.js
+++ packages/ui/index.js
@@ -3,6 +3,7 @@ const { colors } = require("@georgewrmarshall/design-tokens-test");
 
 const Button = () => {
   console.log(`Button styled with primary color: ${colors.primary}`);
+  console.log(`Button styled with secondary color: ${colors.secondary}`);
 };
 
 Button();
